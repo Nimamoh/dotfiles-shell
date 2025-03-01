ea64b1b88621096f7780cd3bfa447dc1c786cb5e
Nimamoh <nimamoh@nimamoh.net>
2025-03-01 17:29:40 +0100

    update dreadnough

--- shell/.local/cross_shell_conf.dreadnough.sh
+++ shell/.local/cross_shell_conf.dreadnough.sh
@@ -11,22 +11,18 @@ export VISUAL="code --wait"
 # export BROWSER=microsoft-edge
 export BROWSER="'/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'"
 
-## Enable gpg and ssh relay to use gpg agent on windows along with ssh-pageant.
-relay() {
-  export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+## Enable gpg and ssh agent
+export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+echo >&2 "For using your yubikey on this machine:"
+echo >&2 "  From powershell admin:"
+echo >&2 "  usbipd list"
+echo >&2 "  usbipd attach --wsl --busid=8-1 # check it's available through lsusb"
+echo >&2 "  then:"
+echo >&2 "  sudo service pcscd start"
+echo >&2 "  gpg --card-edit"
+echo >&2 "  gpg > verify"
+echo >&2 "  You should be able to use smartcard as ssh key"
 
-  local relayScript
-  relayScript="$HOME/.local/bin/win-gpg-agent-relay"
-  export WINGPGAGENT_DIR="/mnt/f/win-gpg-agent"
-  export WINGPGAGENT_SOCKETS_WINDIR="F:/win-gpg-agent/gnupg"
-  printf >&2 "%-50s" " Starting win-gpg-agent-relay..."
-  if ! ($relayScript status >/dev/null 2>&1 || { timeout 1 "$relayScript" start; sleep 0.1; } >/dev/null 2>&1); then
-    echo >&2 "❌"
-  else
-    echo >&2 "✅"
-  fi
-}
-relay
 
 ### Mount pre-defined locations
 while IFS= read -r -d '' f; do
