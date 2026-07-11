be37629fbc8d0a08b9da5a197917d7cdd5bc4d7d
Romain Gautier <romain.gautier@nimamoh.net>
2026-07-11 18:11:22 +0200

    update dotfiles for dreadarch

--- shell/.local/cross_shell_conf.dreadarch.sh
+++ shell/.local/cross_shell_conf.dreadarch.sh
@@ -13,25 +13,15 @@ export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 echo >&2 "For using your yubikey on this machine:"
 echo >&2 "  From powershell admin:"
 echo >&2 "  usbipd list"
-echo >&2 "  usbipd attach --wsl --busid=8-1 # check it's available through lsusb"
-echo >&2 "  then:"
-echo >&2 "  sudo service pcscd start"
-echo >&2 "  gpg --card-edit"
-echo >&2 "  gpg > verify"
+echo >&2 "  usbipd attach --wsl archlinux --busid=8-1 # check it's available through lsusb"
 echo >&2 "  You should be able to use smartcard as ssh key"
 echo >&2
 
-
-### Mount pre-defined locations
-# while IFS= read -r -d '' f; do
-#   printf >&2 "%-50s" " Mounting $f ... "
-#   if ! (mountpoint "$f" >/dev/null 2>&1 || timeout 0.5 mount "$f" >/dev/null 2>&1); then
-#     echo >&2 -e "❌"
-#     continue
-#   else
-#     echo >&2 -e "✅"
-#   fi
-# done < <(find /mnt -maxdepth 1 -type d -name "*CIFS" -print0)
+#
+# Il faut mettre à jour le tty pour les confirmations pinentry
+#
+export GPG_TTY=$(tty)
+gpg-connect-agent updatestartuptty /bye
 
 ### Various check and sugar
 if timeout 0.5 pgrep docker >/dev/null 2>&1; then # grep the process instead of calling docker directly for speed
